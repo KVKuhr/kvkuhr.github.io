c955b8064b0a23e4eaf30096a0a0bb8b9227469d
Klaus Victor Kühr <kvkuhr@gmail.com>
2019-01-31 14:52:32 -0200

    Gallery - Fin

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,32 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+        // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.alt;
+
+          mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      //Set current
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+
+
+
+    });
+  });
 }
